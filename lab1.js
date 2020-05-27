1fe3d159785fdce1d2c6e752c49d60edaf489adf
InnaAlexeeva <59958263+InnaAlexeeva@users.noreply.github.com>
2020-05-27 12:30:48 +0300

    Create lab1.js

---
+++ lab1.js
@@ -0,0 +1,82 @@
+
+
+	
+<!DOCTYPE html>
+ <html>
+     <head>
+< title > START </ title > 
+var colors = [ '#FF0000'],
+    el = $('#000080'), // элемент, у которого меняем цвет
+    i = 0;
+ 
+setInterval(function(){
+    el.css('color', colors[i]);
+    if((++i) >= 2) {
+        i = 1;
+        colors.reverse();
+    }
+}, 2,5000);
+      } 
+       </ style > 
+
+         <script type="text/javascript">
+         function changecolors() {
+             x = 2,5;
+             var t = setTimeout("change()", 2,5000);
+         }
+         function change() {
+             while(x < 3) {
+                 if(x = 2,5) {
+                     color = " Blue";
+                     x++;
+       < h2 > START </ h2 > 
+
+                 } else if (x = 2) {
+                     color = "red";
+                     x = 2,5;
+                 }
+                 document.body.style.background = color;
+(() => {
+const elements = document.getElementsByClassName ('child');
+const rec = (arr) => {
+if (! arr.length) return;
+const [even, ... rest] = arr;
+even.setAttribute ('style', 'color: yellow');
+return rec (rest);
+}
+Rec (elements);
+return;
+}) ();
+function timedText() {
+  var x = document.getElementById("txt");
+  setTimeout(function(){ x.value=" 1 5 seconds" }, 5000);
+  setTimeout(function(){ x.value=" 2 5 seconds" }, 5000);
+  setTimeout(function(){ x.value="3 5 seconds" }, 5000);
+  setTimeout(function(){ x.value=" 4 5 seconds" }, 5000);
+  setTimeout(function(){ x.value=" 5 5 seconds" }, 5000);
+  setTimeout(function(){ x.value="6 5 seconds" }, 5000);  
+setTimeout(function(){ x.value=" 7 5 seconds" }, 5000);
+  setTimeout(function(){ x.value=" 8 5 seconds" }, 5000);
+  setTimeout(function(){ x.value="9 5 seconds" }, 5000);
+setTimeout(function(){ x.value=" 10 5 seconds" }, 5000);
+  setTimeout(function(){ x.value=" 11 5 seconds" }, 5000);
+  
+
+  }
+           }
+         }
+     </head>
+     <body onload="changecolors()">
+setTimeout(function(){
+  if ($('#divID').length > 0) {
+    $('#divID').remove();
+  }
+}, 15000)
+<script type="text/javascript">
+
+document.write("КОНЕЦ");
+</script>
+
+    </body>
+ </html>
+
